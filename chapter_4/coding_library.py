a785aa609b074137da0d7428a4322a82f9f8f364
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-03-16 13:11:58 +0000

    working with fourth chapter

--- chapter_4/coding_library.py
+++ chapter_4/coding_library.py
@@ -22,7 +22,7 @@ class StockOption():
         self.df = math.exp(-(r-self.div)*self.dt)
 
 class BinomialEuropeanOption(StockOption):
-    def __setup_parameters__(self):
+    def _setup_parameters_(self):
         self.M = self.N+1
         self.u = 1+self.pu
         self.d = 1-self.pd
@@ -52,14 +52,14 @@ class BinomialEuropeanOption(StockOption):
         return self._travere_tree_(payoffs)
     
     def price(self):
-        self.__setup_parameters__()
+        self._setup_parameters_()
         self._initialize_stock_price_tree_()
         payoffs = self.__begin_tree_traversal__()
 
         return payoffs[0]
     
 class BinomialTreeOption(StockOption):
-    def __setup_parameters__(self):
+    def _setup_parameters_(self):
         self.u = 1+self.pu
         self.d = 1-self.pd
         self.qu = (math.exp((self.r-self.div)*self.dt)-self.d)/(self.u-self.d)
@@ -94,10 +94,66 @@ class BinomialTreeOption(StockOption):
         return self._traverse_tree_(payoffs)
     
     def price(self):
-        self.__setup_parameters__()
+        self._setup_parameters_()
         self._initialize_stock_price_tree_()
         payoffs = self.__begin_tree_traversal__()
         return payoffs[0]
     
+class BinomialCRROption(BinomialTreeOption):
+    def _setup_parameters_(self):
+        self.u = math.exp(self.sigma * math.sqrt(self.dt))
+        self.d = 1./self.u
+        self.qu = (math.exp((self.r-self.div)*self.dt)-self.d)/(self.u-self.d)
+        self.qd = 1-self.qu
+        return
+
+class BinomialLROption(BinomialTreeOption):
+    def _setup_parameters_(self):
+        odd_N = self.N if (self.N%2 == 1) else (self.N+1)
+        d1 = (math.log(self.S0/self.K)+((self.r-self.div)+(self.sigma**2)*0.5)*self.T)/(self.sigma*math.sqrt(self.T))
+        d2 = (math.log(self.S0/self.K)+((self.r-self.div)-(self.sigma**2)*0.5)*self.T)/(self.sigma*math.sqrt(self.T))
+
+        pp2_inversion = lambda z,n:.5+math.copysign(1,z)*math.sqrt(.25- 0.25*math.exp(-((z/(n+1./3 +.1/(n+1)))**2.)*(n+1./6)))
+        pbar = pp2_inversion(d1, odd_N)
+
+        self.p = pp2_inversion(d2, odd_N)
+        self.u = 1/self.df * pbar/self.p
+        self.d = (1/self.df - self.p*self.u)/(1-self.p)
+        self.qu = self.p
+        self.qd = 1-self.p
+
+class BinomialLRWithGreeks(BinomialLROption):
+    def __new_stock_price_tree__(self):
+        self.STs = [np.array([self.S0*self.u/self.d, self.S0, self.S0*self.d/self.u])]
+
+        for i in range(self.N):
+            prev_branches = self.STs[-1]
+            st = np.concatenate((prev_branches*self.u, [prev_branches[-1]*self.d]))
+            self.STs.append(st)
+        return 
+    
+    def price(self):
+        self._setup_parameters_()
+        self.__new_stock_price_tree__()
+        payoffs = self.__begin_tree_traversal__()
+
+        option_value = payoffs[1]
+        payoff_up = payoffs[0]
+        payoff_down = payoffs[-1]
+
+        S_up = self.STs[0][0]
+        S_down = self.STs[0][-1]
+
+        dS_up = S_up - self.S0
+        dS_down = self.S0 - S_down
+
+        dS = S_up-S_down
+        dV = payoff_up-payoff_down
+
+        delta = dV/dS
+
+        gamma = ((payoff_up-option_value)/dS_up - (option_value-payoff_down)/dS_down)/((self.S0 + S_up)*0.5 - (self.S0+S_down)*0.5)
+        return option_value, delta, gamma
+
 if __name__ == "__main__":
     pass
