80a124996db5b41855938eff11b4872815b7cc31
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-03-17 11:59:03 +0000

    preparing the fifth chapter

---
+++ chapter_5.md
@@ -0,0 +1,3 @@
+# Chapter 5 - Interest Rates and Derivatives
+
+## Summary
---
+++ chapter_5/coding_library.py
@@ -0,0 +1,10 @@
+#pylint: disable-all
+import math
+import numpy as np
+import scipy.linalg as linalg
+import sys
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    pass
