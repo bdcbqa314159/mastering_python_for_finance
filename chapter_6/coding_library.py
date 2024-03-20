f49f4aa6eae3957638ec33cb9c51e7b304860529
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-03-20 15:01:24 +0000

    preparing the sixth chapter

---
+++ chapter_6/coding_library.py
@@ -0,0 +1,9 @@
+#pylint: disable-all
+import math
+import numpy as np
+import scipy.optimize as optimize
+import scipy.stats as st
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    pass
